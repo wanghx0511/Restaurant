a5330401142d362c649b7ce496506b087d489a25
wanghx0511 <huixin.wang@foxmail.com>
2017-05-09 02:13:12 +0800

    优化

--- bin/js/LayaSample.js
+++
@@ -1,9 +0,0 @@
-// 程序入口
-var GameMain = (function () {
-    function GameMain() {
-        Laya.init(600, 400);
-    }
-    return GameMain;
-}());
-new GameMain();
-//# sourceMappingURL=LayaSample.js.map
