da6190d71f9d3b92402e4a44944c8e97f8f139f7
sin-wang-91 <sayfarewell1@163.com>
2017-05-21 16:58:35 +0800

    垃圾桶

--- src/stage/Trash.ts
+++ src/stage/Trash.ts
@@ -23,7 +23,7 @@ class Trash extends Laya.Sprite{
     public abandon(item: Item) {
         //销毁
         item.destroy();
-        this.trashCan.graphics.destroy();
+        this.trashCan.graphics.clear();
         this.trashCan.loadImage("stage/lajitong2.png");
         Laya.timer.once(800, this, this.onChangeImage);
 
@@ -38,7 +38,7 @@ class Trash extends Laya.Sprite{
     }
 
     private onChangeImage(){
-        this.trashCan.graphics.destroy();
+        this.trashCan.graphics.clear();
         this.trashCan.loadImage("stage/lajitong.png");
     }
 
